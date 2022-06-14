0e05fe408ad888c9dd570b784b63e691e923e51e
Haneda_Tomo <c0a2110889@edu.teu.ac.jp>
2022-06-14 19:11:25 +0900

    a

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,9 +1,6 @@
 import random
 from timeit import repeat
-from sympy import Chi
 
-
-ward = list("ABCDEFGHIJKLMNOPQRSTUVWXYG")
 kurikaesi = 5 #繰り返し
 taisyou = 10 #対象
 keson = 2 #欠損
@@ -15,6 +12,7 @@ def  main():
         
 
 def mondai():
+      ward = [chr(c+65) for c in range(26)]
       char = random.sample(ward,taisyou)#選択
       print(char)
       char_K = random.sample(char,keson)#欠損
@@ -27,8 +25,16 @@ def mondai():
 def seikai():
     kazu = int(input("欠損文字はいくつ？"))
     if kazu == keson:
-        print("正解")
+        print("正解です")
+        for i in range(kazu):
+            c = input(f"{i+1}一つ目の文字を入力してください")
+            if c not in seikai:
+                print("不正解です、またチャレンジしてください")
+            return 0
+        return 1
     else:
         print("不正解")
+        return 0
 
-
+if __name__ == " __main__":
+    main()
