7ebf74f936534c22f22c83144aff19f7721f5738
Haneda_Tomo <c0a2110889@edu.teu.ac.jp>
2022-06-21 15:11:39 +0900

    問題5

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,24 +1,35 @@
+from cmath import e
+from html import entities
 import tkinter as tk
 import tkinter.messagebox as tkm
 print(__name__)
 
+def button_click(event):
+    btn = event.widget
+    num = btn["text"]
+    #tkm.showinfo("",f"{num}のボタンが押されました")
+    entry.insert(tk.END, num)
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("電卓")
-    root.geometry("300x500")
+    #root.geometry("300x500")
       
-    r,c = 0, 0
-    for num in range(9, -1, -1):
+    entry = tk.Entry(root, justify="right",width=10,font=("Times New Roman",40))
+    entry.grid(row=0,column=0,columnspan=3) 
+    
+    r,c = 1, 0
+    for i, num in enumerate([i for i in range(9, -1, -1)]+["+"]):
         btn = tk.Button(root,
-                        text=f"{num}",
+                        text=num,
                         width=4,
                         height=2,
                         font=("Times New Roman",30)
                     )
-        btn.grid(row=r, column=c)
+        btn.bind("<1>",button_click)
+        btn.grid(row=r,column=c)
         c += 1
-        if (num-1)%3 == 0:
+        if (i+1)%3 == 0:
             r += 1
             c = 0
 
