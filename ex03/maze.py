dc9ff110bd4502451e5a3fae6795abd5b2d0a18b
Haneda_Tomo <c0a2110889@edu.teu.ac.jp>
2022-06-28 16:51:21 +0900

    maze追加

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,22 +1,33 @@
 
+import tkinter.messagebox as tkm
 import tkinter as tk
 import maze_maker as mm
+
+global cl
+cl = True
+
 def key_down(event):
     global key
     key = event.keysym
     
 def key_up(event):
-    global key
+    global key ,cl
     key=""
+    if cx==1350 and cy==750 and cl==True :#ゴールのポップアップ
+        tkm.showwarning(root,"  ゴール  ")
+        cl = None
 
 def main_proc(): #こうかとんの移動
     global cx, cy,my,mx
-    delta = {"":[0,0],"Up":[0,-1],"Down":[0,+1],"Left":[-1,0],"Right":[+1,0]}
+    delta = {"":[0,0],"Up":[0,-1],"s":[0,+1],"a":[-1,0],"d":[+1,0]}
     
     # cx,cy = cx+delta[key][0], cy+delta[key][1]
 
-    if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0:
+    if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0:#壁の中に入らないようにする
         my,mx = my+delta[key][1],mx+delta[key][0]
+    else:
+        pass
+    
     cx,cy = mx*100+50,my*100+50
     cabvas.coords("tori",cx,cy)
     root.after(100,main_proc)
@@ -26,8 +37,10 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
 
-    cabvas = tk.Canvas(root,width=1500,height=900,bg="brown" )
+    cabvas = tk.Canvas(root,width=1500,height=900)
+    cabvas.create_rectangle(100,100,100+100,100+100,fill="red")
     cabvas.pack()
+
     maze_bg = mm.make_maze(15,9)#
     mm.show_maze(cabvas,maze_bg)#
     mx,my = 1,1
